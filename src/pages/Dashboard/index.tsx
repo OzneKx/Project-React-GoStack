c8389f970dc687ba11e1e26a975ec1f6976a780b
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-09-01 20:56:27 -0300

    First React Project - GoStack

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,28 @@
+/* eslint-disable arrow-body-style */
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo-github.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
